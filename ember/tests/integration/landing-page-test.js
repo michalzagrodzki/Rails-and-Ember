6e52d019212f444be616de4edb3ef0c60206c340
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-05-25 19:08:34 +0200

    Add test for home navigation link in Application View. Tests passed.

--- ember/tests/integration/landing-page-test.js
+++ ember/tests/integration/landing-page-test.js
@@ -14,9 +14,18 @@ module('Integration - Landing Page', {
   }
 });
 
-// test presence of text on application page
+// test for presence of text on application page
 test('Should welcome me to Presentation Ember App', function(assert){
   visit('/').then(function(){
     assert.equal(find('h2#title').text(), 'Welcome to Presentation Ember App');
+  });
+});
+
+// test for presence of back button, after clicking link 'home' check if header h3 changed from About into something else.
+test('Should allow to navigate back to main page', function(assert){
+  visit('/about').then(function(){
+    click('a:contains("Home")').then(function(){
+      assert.notEqual(find('h3').text(), 'About');
+    });
   });
 });
