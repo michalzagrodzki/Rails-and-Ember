4fdff24373f34a86c1b720cce4dbb29b0f37ce19
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-05-28 11:59:43 +0200

    Add View for Speakers Index, add routes, add links in application View. Test passed.

--- ember/app/router.js
+++ ember/app/router.js
@@ -7,6 +7,7 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('about'); // route for static page 'About'
+  this.resource('speakers'); // route for 'Speakers' index page
 });
 
 export default Router;
